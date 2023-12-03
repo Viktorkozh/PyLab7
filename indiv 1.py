335ec94fd4cf16747dc5cb4ff96ec239ff2e7c8e
ViktorKozh <viktor.ru253@gmail.com>
2023-12-03 22:34:25 +0300

    Last

---
+++ indiv 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == '__main__':
+    # ввести список одной строкой.
+    A = list(map(int, input().split()))
+    # Проверить количество элементов списка.
+    if len(A) < 2:
+        print("Неверный размер списка", file=sys.stderr)
+        exit(1)
+
+    # найти искомую сумму.
+    S = 0
+    for item in A:
+        if item % 2 == 0:
+            S += item
+
+    print(S)
---
+++ indiv 2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+if __name__ == '__main__':
+    # Ввести список одной строкой.
+    a = list(map(float, input().split()))
+    a_modified = a
+    # Если список пуст, завершить программу.
+    if not a:
+        print("Заданный список пуст", file=sys.stderr)
+        exit(1)
+
+    count = 0
+    # Определить индексы минимального и максимального элементов.
+    for i, item in enumerate(a):
+        if item == 0:
+            i_zero = i
+        if item > 0:
+            count += 1
+        if int(item) <= 1:
+            poped = a_modified.pop(i)
+            a_modified.insert(0, poped)
+
+    a_cut = a[i_zero + 1:]
+    S = 0
+    for item in a_cut:
+        S += item
+
+    print("Kоличество положительных элементов списка: ", count)
+    print("Cуммa элементов списка, расположенных после последнего элемента, равного нулю: ", S)
+    print("Преобразованный список, где сначала располагаются все элементы, целая часть которых не превышает 1, а потом - все остальные: ", a_modified)
